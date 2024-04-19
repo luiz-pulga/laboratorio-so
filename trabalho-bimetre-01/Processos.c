b7a06f9d2f357a95bcde2bb2e5b93b5a45179372
Luiz Pulga <162224053+luiz-pulga@users.noreply.github.com>
2024-04-19 20:53:19 -0300

    Update Processos.c

--- trabalho-bimetre-01/Processos.c
+++ trabalho-bimetre-01/Processos.c
@@ -8,82 +8,76 @@ int sentido_atual;
 int tempo_corrente;
 
 int funcionamento_escada() {
+    int sentido_oposto;
+    int posicoes[2] = {0, 0};
+    int tempo_ultima_pessoa = 0;
 
-  // Definição de variáveis locais
-  int sentido_oposto;
-  int posicoes[2] = {0, 0};
-  int tempo_ultima_pessoa = 0;
+    tempo_corrente = agenda[sentido_atual][0];
 
-  tempo_corrente = agenda[sentido_atual][0];
+    while (posicoes[0] < contagens[0] || posicoes[1] < contagens[1]) {
+        sentido_oposto = 1 - sentido_atual;
 
-  while (posicoes[0] < contagens[0] || posicoes[1] < contagens[1]) {
-    sentido_oposto = 1 - sentido_atual;
-
-    if (agenda[sentido_atual][posicoes[sentido_atual]] <= tempo_ultima_pessoa) {
-      tempo_corrente = agenda[sentido_atual][posicoes[sentido_atual]];
-      posicoes[sentido_atual]++;
-    } 
-    else {
-      int proximo_tempo_oposto = agenda[sentido_oposto][posicoes[sentido_oposto]];
-      tempo_corrente = (tempo_ultima_pessoa > proximo_tempo_oposto) ? tempo_ultima_pessoa : proximo_tempo_oposto;
-      posicoes[sentido_oposto]++;
+        if (posicoes[sentido_atual] < contagens[sentido_atual] && (agenda[sentido_atual][posicoes[sentido_atual]] <= tempo_ultima_pessoa || posicoes[sentido_oposto] >= contagens[sentido_oposto])) {
+            tempo_corrente = agenda[sentido_atual][posicoes[sentido_atual]];
+            tempo_ultima_pessoa = tempo_corrente + 10;
+            posicoes[sentido_atual]++;
+        } else if (posicoes[sentido_oposto] < contagens[sentido_oposto]) {
+            tempo_corrente = agenda[sentido_oposto][posicoes[sentido_oposto]];
+            tempo_ultima_pessoa = tempo_corrente + 10;
+            posicoes[sentido_oposto]++;
+            sentido_atual = sentido_oposto;
+        }
     }
 
-    tempo_ultima_pessoa = tempo_corrente + 10;
-    sentido_atual = 1 - sentido_atual;
-  }
-
-  return tempo_ultima_pessoa + 10;
+    return tempo_ultima_pessoa;
 }
 
 int main() {
+    FILE *entrada, *saida;
 
-  FILE *entrada, *saida;
-
-  entrada = fopen("input.txt", "r");
-  if (entrada == NULL) {
-    printf("Erro ao abrir arquivo de entrada.\n");
-    return 0;
-  }
-
-  int quantidade_pessoas;
-  fscanf(entrada, "%d", &quantidade_pessoas);
-
-  int instante, direcao;
-  for (int i = 0; i < quantidade_pessoas; i++) {
-    fscanf(entrada, "%d %d", &instante, &direcao);
-
-    if (i == 0) {
-      sentido_atual = direcao;
+    entrada = fopen("input.txt", "r");
+    if (entrada == NULL) {
+        printf("Erro ao abrir arquivo de entrada.\n");
+        return 0;
     }
 
-    agenda[direcao][contagens[direcao]] = instante;
-    contagens[direcao]++;
-  }
+    int quantidade_pessoas;
+    fscanf(entrada, "%d", &quantidade_pessoas);
 
-  fclose(entrada);
+    int instante, direcao;
+    for (int i = 0; i < quantidade_pessoas; i++) {
+        fscanf(entrada, "%d %d", &instante, &direcao);
 
-  pid_t pid = fork();
+        if (i == 0) {
+            sentido_atual = direcao;
+        }
 
-  if (pid == 0) {
-    int resultado = funcionamento_escada();
-    return 0;
-  } else {
-    wait(NULL);
-
-    saida = fopen("output.txt", "w");
-    if (saida == NULL) {
-      printf("Erro ao abrir arquivo de saída.\n");
-      return 0;
+        agenda[direcao][contagens[direcao]] = instante;
+        contagens[direcao]++;
     }
 
-    int resultado_final = funcionamento_escada();
-    fprintf(saida, "%d\n", resultado_final);
-    fclose(saida);
+    fclose(entrada);
 
-    printf("%d\n", resultado_final);
-  }
+    pid_t pid = fork();
 
+    if (pid == 0) {
+        exit(funcionamento_escada());
+    } else {
+        int status;
+        wait(&status);
 
-return 0;
+        saida = fopen("output.txt", "w");
+        if (saida == NULL) {
+            printf("Erro ao abrir arquivo de saída.\n");
+            return 0;
+        }
+
+        // A saída é o tempo da última pessoa adicionado de 10 segundos para finalizar a última subida
+        fprintf(saida, "%d\n", WEXITSTATUS(status));
+        fclose(saida);
+
+        printf("O tempo final é: %d segundos.\n", WEXITSTATUS(status));
+    }
+
+    return 0;
 }
